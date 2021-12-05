603eec1464fbefc72c2af675393107a14b62c4f7
prakash <prakash.kerinfotech@gmail.com>
2021-12-05 16:13:19 +0530

    commit

---
+++ inc/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$GLOBALS['site_url'] = 'http://localhost/prakash/PHP/PHP-CRUD/';
+$GLOBALS['assets_url'] = 'http://localhost/prakash/PHP/PHP-CRUD/assets/';
+
+
+include_once('inc/functions.php');
